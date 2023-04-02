65ff9aefb8ebf891a47a5ca975940f1f6298e078
MrGREAT20 <pralaywandre20@gmail.com>
2023-04-02 18:39:44 +0530

    Setup bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverconfig.js');
 const setupAndStartServer = async() =>{
     //create a express object
     const app = express();
     //const PORT = 3000; //abhi yeh PORT number, humare liye 3000 lekin kisi or ke isme yeh PORT khali nhi ho sakta, toh hum
     //isko config folder me daal sakte hai shayad
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, ()=>{
         console.log(`Server running at ${PORT}`);
     })
